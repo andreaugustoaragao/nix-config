eecbb6b779c2cccd8d27035a518b718f2c803762
andreaugustoaragao <andrearag@gmail.com>
2024-08-15 12:17:20 -0600

    changes to i3 and polybar

--- home/xdg.nix
+++ home/xdg.nix
@@ -8,26 +8,22 @@
     url = "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/chatgpt-icon.png";
     sha256 = "157xbcxzlfjb9ls7vja9vq8zb4i8nkd33qk58qva7yavn71zzxay";
   };
-
   #redditIcon = pkgs.fetchurl {
   #  url = "https://cdn.icon-icons.com/icons2/1195/PNG/512/1490889653-reddit_82537.png";
   #  sha256 = "01ja74q5i797s0cfhr8byqq1bzzix23hswimij663ylm864w7lna";
   #};
-
   #twitterIcon = pkgs.fetchurl {
   #  url = "https://cdn.icon-icons.com/icons2/836/PNG/512/Twitter_icon-icons.com_66803.png";
   #  sha256 = "1mlqxxj2rwwv439lvdv4k4djhmwk92lv1riywk94r9hcmk5bbs92";
   #};
-
   #vimCheatSheetIcon = pkgs.fetchurl {
   #  url = "https://cdn.icon-icons.com/icons2/1381/PNG/512/vim_94609.png";
   #  sha256 = "0fnrcrsrrnchrgjbg0hszynj2g2m674b3nc4ky8pdb3zgc1490sc";
   #};
-
-  youtubeMusicIcon = pkgs.fetchurl {
-    url = "https://cdn.icon-icons.com/icons2/3132/PNG/512/youtube_music_social_network_song_multimedia_icon_192250.png";
-    sha256 = "0hxwh8x4xmpa9rpmscds9sip08a6xz9s58xncd2mlnyzh8pa447b";
-  };
+  #youtubeMusicIcon = pkgs.fetchurl {
+  #  url = "https://cdn.icon-icons.com/icons2/3132/PNG/512/youtube_music_social_network_song_multimedia_icon_192250.png";
+  #  sha256 = "0hxwh8x4xmpa9rpmscds9sip08a6xz9s58xncd2mlnyzh8pa447b";
+  #};
 in {
   home.packages = with pkgs; [
     xdg-utils # provides cli tools such as `xdg-mime` `xdg-open`
@@ -119,7 +115,7 @@ in {
       name = "Youtube Music";
       genericName = "Youtube Music";
       exec = "brave -app=https://music.youtube.com";
-      icon = youtubeMusicIcon;
+      icon = "youtube-music"; #youtubeMusicIcon;
     };
 
     youtube = {
